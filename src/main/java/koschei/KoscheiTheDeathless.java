d2e389f846c7fe892bf5c57fee9166ef66456a60
Trewas345 <vazheninsaha@gmail.com>
2024-08-22 02:36:42 +0500

    2.1.4

---
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -0,0 +1,17 @@
+package koschei;
+
+import koschei.models.Ocean1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KoscheiTheDeathless {
+
+    @Autowired
+    private Ocean1 ocean;
+
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean.toString();
+    }
+
+}
